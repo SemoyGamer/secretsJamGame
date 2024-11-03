9a48fb4affb9d5f7a114d2612902207646ed662e
SemoyGamer <samuelm01@comcast.net>
2024-11-02 22:32:56 -0400

    Player Movement and Collisions

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -4,34 +4,18 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
+		//get input direction and move the player
+		Vector2 direction = Input.GetVector("left", "right", "up", "down");
+		if (direction != Vector2.Zero){
+			velocity = direction.Normalized() * Speed;
 		}
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		else{
+			velocity = Vector2.Zero;
 		}
 
 		Velocity = velocity;
