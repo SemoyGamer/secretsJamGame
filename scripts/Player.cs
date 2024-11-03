53d34c5b329e5f3a12039b95ca005aec3595de1b
SemoyGamer <samuelm01@comcast.net>
2024-11-03 00:49:29 -0400

    Added a Camera

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,16 +1,16 @@
 using Godot;
 using System;
 
-public partial class Player : CharacterBody2D
-{
-	public const float Speed = 300.0f;
+public partial class Player : CharacterBody2D{
+	public const float Speed = 400.0f;
 
-	public override void _PhysicsProcess(double delta)
-	{
+	Vector2 direction;
+
+	public override void _PhysicsProcess(double delta){
 		Vector2 velocity = Velocity;
 
 		//get input direction and move the player
-		Vector2 direction = Input.GetVector("left", "right", "up", "down");
+		direction = Input.GetVector("left", "right", "up", "down");
 		if (direction != Vector2.Zero){
 			velocity = direction.Normalized() * Speed;
 		}
