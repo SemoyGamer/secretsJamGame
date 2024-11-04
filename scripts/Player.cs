4196038bcaadbc19637036c48f6a0f48a145f68b
SemoyGamer <samuelm01@comcast.net>
2024-11-04 17:50:15 -0500

    Smooth Player Rotation and Animation

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -6,19 +6,48 @@ public partial class Player : CharacterBody2D{
 
 	Vector2 direction;
 
+	AnimatedSprite2D playerSprite;
+
+	//smooth rotation values
+	public float _theta;
+	public float rotationSpeed = Mathf.Tau * 2;
+
+	public override void _Ready(){
+		//Add a value to some of the variables
+		playerSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
+
 	public override void _PhysicsProcess(double delta){
 		Vector2 velocity = Velocity;
 
-		//get input direction and move the player
+		//get input direction and move the player, also play the walk and idle animations
 		direction = Input.GetVector("left", "right", "up", "down");
 		if (direction != Vector2.Zero){
 			velocity = direction.Normalized() * Speed;
-		}
-		else{
+			playerSprite.Play("walking");
+
+			//player rotation
+			_theta = Mathf.Wrap(Mathf.Atan2(direction.Y, direction.X) - Rotation + Mathf.Pi / 2, -Mathf.Pi, Mathf.Pi);
+			Rotation += Mathf.Clamp(rotationSpeed * (float)delta, 0, Mathf.Abs(_theta)) * Mathf.Sign(_theta);
+			//float aRot = Rotation - 90;
+		}else{
 			velocity = Vector2.Zero;
+			playerSprite.Play("idle");
 		}
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+
+	public override void _Process(double delta){
+		//setPlayerRotation();
+	}
+
+	public void setPlayerRotation(){
+		if(direction == new Vector2 (0, -1)){
+			//Rotation.LerpAngle(5,0);
+		}else if(direction == new Vector2 (0, 1)){
+			RotationDegrees = 180;
+		}
 	}
 }
