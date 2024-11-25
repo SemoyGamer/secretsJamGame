a87e3de211f71b46143a8863ca4b03034c13b3ed
SemoyGamer <samuelm01@comcast.net>
2024-11-24 22:14:28 -0500

    Part of End Cutscene

---
+++ scripts/LargeHeater.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System;
+
+public partial class LargeHeater : AnimatedSprite2D
+{
+	AnimatedSprite2D fakePlayer;
+	Timer walkTimer;
+
+	bool isWalking = false;
+
+	public override void _Ready()
+	{
+		fakePlayer = GetNode<AnimatedSprite2D>("fakePlayer");
+		walkTimer = GetNode<Timer>("walkTimer");
+
+		fakePlayer.Visible = false;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		if(isWalking){
+			fakePlayer.Position += new Vector2(0, -3);
+			fakePlayer.GetNode<Camera2D>("Camera2D").Position += new Vector2(0, -0.5f);
+		}
+	}
+
+	public void _on_area_2d_body_entered(Node2D body){
+		if(body.IsInGroup("player")){
+			body.QueueFree();
+			body.GetNode<Camera2D>("Camera2D").Enabled = false;
+			fakePlayer.GetNode<Camera2D>("Camera2D").Enabled = true;
+			fakePlayer.Visible = true;
+			isWalking = true;
+
+			walkTimer.Start();
+		}
+	}
+
+	public void _on_walk_timer_timeout(){
+		isWalking = false;
+		fakePlayer.Play("idle");
+		Play("waterGone");
+		GetNode<AnimatedSprite2D>("offButton").Play("pressed");
+		GetNode<AudioStreamPlayer2D>("waterFlowSound").Stop();
+	}
+}
